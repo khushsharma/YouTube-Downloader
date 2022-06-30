2ec470427e6cbff0b121f10c105818f646534f6f
Khush Sharma <whyiamkhush@gmail.com>
2022-07-01 01:18:47 +0530

    hover seffect

--- download.py
+++ download.py
@@ -1,11 +1,23 @@
 from os import path
 from tkinter import *
 from tkinter import filedialog
+from turtle import color
 from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
+from custombutton import RoundedButton
+from turtle import color
 
-
+def changeOnHover(button, colorOnHover, colorOnLeave):
+  
+    # adjusting backgroung of the widget
+    # background on entering widget
+    button.bind("<Enter>", func=lambda e: button.config(
+        background=colorOnHover))
+  
+    # background color on leving widget
+    button.bind("<Leave>", func=lambda e: button.config(
+        background=colorOnLeave))
 
 def select_path():
     # allow user to select path from the explorer
@@ -25,8 +37,9 @@ def download_file():
 
 screen = Tk()
 title = screen.title('YouTube Downloader')
-canvas = Canvas(screen,width = 600,height= 600)
+canvas = Canvas(screen,width = 600,height= 600 )
 canvas.pack()
+
 
 #image logo
 logo_img = PhotoImage(file='yt.png')
@@ -41,8 +54,10 @@ link_label = Label(screen,text="Enter the Download link" ,font=('Arial ' ,15))
 
 #select path for saving file
 path_label = Label(screen,text="Select Path For Download",font=('Arial ' ,15))
-select_btn = Button(screen , text = "Select" ,font=('Arial ' ,10),command=select_path)
-
+#select_btn = Button(screen , text = "Select" ,font=('Arial ' ,20),command=select_path)
+select_btn = RoundedButton(screen, text="Select", border_radius=5, padding=20, command=select_path, color="#bfe3e1" ,fsize=20)
+#effects
+changeOnHover(select_btn,'#8aa2a1','#bfe3e1')
 #Add to window
 canvas.create_window(300,350 ,window= path_label)
 canvas.create_window(300,400 ,window=select_btn)
@@ -53,9 +68,13 @@ canvas.create_window(300,270 ,window = link_label)
 canvas.create_window(300,300 ,window = link_field)
 
 # download buttons
-download_btn = Button(screen , text="Download the File" ,font=('Arial ' ,10) , command= download_file)
+#download_btn = Button(screen , text="Download the File" ,font=('Arial ' ,20) , command= download_file)
+download_btn = RoundedButton(screen, text="Download the File", border_radius=5, padding=20, command=download_file, color="#bfe3e1" ,fsize=20)
+#effects for button
+changeOnHover(download_btn,'#8aa2a1','#bfe3e1')
 
 #Add to canvas
 canvas.create_window(300,450,window = download_btn)
+
 screen.mainloop()
 
